caf86bd035b441ca61144562790c016334efcce7
imoki <78804251+imoki@users.noreply.github.com>
2024-07-13 13:31:06 +0800

    ✨ 新增时间生成规则2
    填写“时间范围”栏目。
    规则2：以&分隔整点。
    如8&10&11，则会依次调整为8:xx、10:xx、11:xx点。

--- CRON.js
+++ CRON.js
@@ -2,28 +2,27 @@
     作者: imoki
     仓库: https://github.com/imoki/
     公众号：默库
-    更新时间：20240712
-    脚本：CRON_INIT.js 初始化程序，自动生成定时任务配置表
-    说明：要运行CRON.js之前，请先运行CRON_INIT脚本。
-          并对表进行配置，第一次运行CRON_INIT会生成wps表，请先填写好wps表的内容。
-          然后再运行一次CRON_INIT表生成CRON表，对CRON表进行配置。
-          “是否调整”选项填“是”则会对其进行时间调整，默认为“否”是排除这个任务不会进行调整
-          如果手动修改了定时任务时间，请重新运行一次CRON_INIT脚本，会自动生成最新的CRON配置表
+    更新时间：20240713
+    脚本：CRON.js 主程序，动态修改定时任务时间
+    说明：再运行此CRON脚本前，请先运行CRON_INIT脚本，并配置好CRON表格的内容。
+          将CRON.js加入定时任务即可自动修改定时任务时间。
+          修改规则为每次往后推1小时
 */
 
+// 修改名称为“wps”表内的值，需要填“wps_sid”和“文档名”
+// wps_sid抓包获得，文档名就是你这个文档的名称
+
+// 不要修改代码，修改wps表表格内的值即可
+var filename = "" // 文件名
+var cookie = ""
+var file_id = file_id // 文件id
+var cronArray = []  // 存放定时任务
 let sheetNameSubConfig = "wps"; // 分配置表名称
 let sheetNameCron = "CRON"
-var cookie = ""
-var taskArray = []
-var headers = ""
-var count = "20" // 读取的文档页数
-// 表中激活的区域的行数和列数
-var row = 0;
-var col = 0;
-var maxRow = 100; // 规定最大行
-var maxCol = 16; // 规定最大列
-var workbook = [] // 存储已存在表数组
-var colNum = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q']
+var pushHM = [] // 记录PUSH任务的推送时间
+var hourMin = 0
+var hourMax = 23
+var line = 100
 
 function sleep(d) {
   for (var t = Date.now(); Date.now() - t <= d; );
@@ -45,57 +44,11 @@ function ActivateSheet(sheetName) {
     return flag;
 }
 
-// 存储已存在的表
-function storeWorkbook() {
-  // 工作簿（Workbook）中所有工作表（Sheet）的集合,下面两种写法是一样的
-  let sheets = Application.ActiveWorkbook.Sheets
-  sheets = Application.Sheets
-
-  // 清空数组
-  workbook.length = 0
-
-  // 打印所有工作表的名称
-  for (let i = 1; i <= sheets.Count; i++) {
-    workbook[i - 1] = (sheets.Item(i).Name)
-    // console.log(workbook[i-1])
-  }
-}
-
-// 判断表是否已存在
-function workbookComp(name) {
-  let flag = 0;
-  let length = workbook.length
-  for (let i = 0; i < length; i++) {
-    if (workbook[i] == name) {
-      flag = 1;
-      console.log("✨ " + name + "表已存在")
-      break
-    }
-  }
-  return flag
-}
-
-// 创建表，若表已存在则不创建，直接写入数据
-function createSheet(name) {
-  // const defaultName = Application.Sheets.DefaultNewSheetName
-  // 工作表对象
-  if (!workbookComp(name)) {
-    Application.Sheets.Add(
-      null,
-      Application.ActiveSheet.Name,
-      1,
-      Application.Enum.XlSheetType.xlWorksheet,
-      name
-    )
-  }
-}
-
-
 // 获取wps_sid、cookie
 function getWpsSid(){
-  // flagConfig = ActivateSheet(sheetNameSubConfig); // 激活wps配置表
+  flagConfig = ActivateSheet(sheetNameSubConfig); // 激活wps配置表
   // 主配置工作表存在
-  if (1) {
+  if (flagConfig == 1) {
     console.log("🍳 开始读取wps配置表");
     for (let i = 2; i <= 100; i++) {
       wps_sid = Application.Range("A" + i).Text; // 以第一个wps为准
@@ -103,91 +56,145 @@ function getWpsSid(){
       break
     }
   }
-  return wps_sid
-  
+  cookie = "wps_sid=" + wps_sid
   // filename = name
 }
 
-// 获取file_id
-function getFile(url){
-  // 查看定时任务
-  resp = HTTP.get(
-    url,
-    { headers: headers }
-  );
-
-  resp = resp.json()
-  // console.log(resp)
-  resplist = resp["list"]
-  let cronlist = ""
-  for(let i = 0; i<resplist.length; i++){
-    roaming = resplist[i]["roaming"]
-    // console.log(roaming)
-    fileid = roaming["fileid"]
-    name = roaming["name"]
-    if(juiceXLSX(name)){
-      console.log("🍳 已找到 " + name + " 文档")
-      cronlist = taskExist(fileid)
-      if(cronlist.length > 0){
-        console.log("🎉 存在定时任务")
-        // console.log(cronlist)
-        for(let i = 0; i < cronlist.length; i++){
-          
-          task = cronlist[i]
-          task_id = task["task_id"]
-          script_id = task["script_id"]
-          script_name = task["script_name"]
-
-          cron_detail = task["cron_detail"]
-          cron_desc = cron_detail["cron_desc"]
-          cron_type = cron_desc["cron_type"]
-          day_of_month = cron_desc["day_of_month"]
-          day_of_week = cron_desc["day_of_week"]
-          // month = cron_desc["month"]
-          hour = cron_desc["hour"]
-          minute = cron_desc["minute"]
-          // year = cron_desc["year"]
-          // file_id = fileid
-          taskArray.push({
-            "filename" : name,
-            "fileid" : fileid,
-            "script_id" : script_id,
-            "script_name" : script_name,
-            "task_id" : task_id,
-            "cron_type":cron_type,
-            "day_of_month": day_of_month,
-            "day_of_week": day_of_week,
-            "hour"  : hour,
-            "minute" : minute,
-          })
-
-        }
-      }
-
-
-      // console.log("🍳 file_id : " + file_id)
-      // break
+// 是否排除文件
+function juiceExclude(script_name){
+  let flagExclude = 0
+  let i = 2
+  let key = Application.Range("I" + i).Text;
+  let keyarry= key.split("&") // 使用|作为分隔符
+  for(let j = 0; j < keyarry.length; j ++){
+    if(script_name == keyarry[j]){ // 默认排除定时任务为CRON 和PUSH的脚本
+      flagExclude = 1
+      console.log( "🍳 排除任务：" , keyarry[j])
+      break
     }
   }
-
-  // console.log(taskArray)
-  sleep(3000)
+  return flagExclude
 }
 
-// 判断是否为xlsx文件
-function juiceXLSX(name){
-  let flag = 0
-  let array= name.split(".") // 使用|作为分隔符
-  if(array.length == 2 && array[1] == "xlsx"){
-    flag = 1
+// 时间范围
+function rangeHM(value){
+  let rule = /~/i;
+  let flagTrue = rule.test(value); // 判断是否存在字符串
+  if (flagTrue == true) {
+    console.log("🍳 使用规则1进行时间生成")
+    return 1
+  } 
+
+  rule = /&/i;
+  flagTrue = rule.test(value); // 判断是否存在字符串
+  if (flagTrue == true) {
+    console.log("🍳 使用规则2进行时间生成")
+    return 2
+  } 
+
+  return 0
+  // let keyarry= value.split("~") // 使用|作为分隔符
+  // // hourMin = keyarry[0]
+  // hourMin = keyarry[0]
+  // hourMax = keyarry[1]
+  // console.log(hourMin, hourMax)
+  // console.log(hourMin)
+  // hourMax = keyarry[1]
+  // minute = keyarry[1]
+}
+
+// 数组字符串转整形
+function arraystrToint(array){
+  let result = []
+  for(let i=0; i<array.length; i++){
+    result.push(parseInt(array[i]))
   }
-  return flag 
+  return result
 }
 
-// 判断是否存在定时任务
-function taskExist(file_id){
-  url = "https://www.kdocs.cn/api/v3/ide/file/" + file_id + "/cron_tasks";
-  // console.log(url)
+// 数组升序排序
+function arraySortUp(value){
+  value.sort(function(a, b) {
+    return a - b; // 升序排序
+  });
+  return value
+}
+
+// 生成时间
+function createTime(hour, minute, hmrange){
+  hour = parseInt(hour)
+  minute = parseInt(minute)
+  
+  // if(hour > 23){
+  //   hour = 0
+  // }
+  // if(minute > 59 || minute < 0){
+  //   minute = 0
+  // }
+
+  // 根据PUSH来智能修改推送时间
+  // console.log(hour, pushHM[0])
+  // 规则1：8~13生成规则
+  // 时分 分别加1
+  let rule = 1
+  rule = rangeHM(hmrange)  // 范围限定函数
+  if(rule == 1){
+    let keyarry= hmrange.split("~") // 使用|作为分隔符
+    // hourMin = keyarry[0]
+    hourMin = keyarry[0]
+    hourMax = keyarry[1]
+
+    hour = hour + 1
+    minute = minute + 1
+    if(hour > hourMax){
+      hour = hourMin
+      // console.log("时间大于PUSH了")
+    }
+    if(minute > 59 || minute < 0){
+      minute = 0
+    }
+
+  }else{
+    // 规则2：8&10&11
+    let keyarry= hmrange.split("&") // 使用|作为分隔符
+    keyarry = arraySortUp(keyarry)  // 升序排序
+    keyarry = arraystrToint(keyarry)  // 转整形
+    // console.log(keyarry)
+    // console.log(hour)
+    let flagChange = 0  // 查看时间是否变化
+    for(let j=0; j < keyarry.length; j++){
+      let hourExpect = keyarry[j]
+      // console.log(hourExpect)
+      if(hour < hourExpect){
+        // 取第一个遇到比原先大的值，就变为它
+        hour = hourExpect
+        flagChange = 1
+        break
+      }
+    }
+
+    // 查找最小值
+
+    if(!flagChange){  // 如果时间没变动， 说明当前时间已经时最大了，则置为最小值
+      hour = parseInt(keyarry[0]) // 则直接置为第一个值
+    }
+
+
+  }
+
+
+  
+
+
+
+    
+  newHM = [hour.toString(), minute.toString()]
+  return newHM
+}
+
+
+// 获取定时任务
+function getTask(url, headers){
   // 查看定时任务
   resp = HTTP.get(
     url,
@@ -198,182 +205,171 @@ function taskExist(file_id){
   // console.log(resp)
   // list -> 数组 -> file_id、task_id、script_name，cron_detail->字典
   cronlist = resp["list"]
-  sleep(3000)
-  return cronlist
+  // console.log(cronlist)
+  for(let i = 0; i < cronlist.length; i++){
+    
+    task = cronlist[i]
+    task_id = task["task_id"]
+    script_id = task["script_id"]
+    script_name = task["script_name"]
+
+    cron_detail = task["cron_detail"]
+    cron_desc = cron_detail["cron_desc"]
+    cron_type = cron_desc["cron_type"]
+    // day_of_month = cron_desc["day_of_month"]
+    // day_of_week = cron_desc["day_of_week"]
+    // month = cron_desc["month"]
+    hour = cron_desc["hour"]
+    minute = cron_desc["minute"]
+    // year = cron_desc["year"]
+
+    let flagExclude = 0;  // 排除掉的文件，1为排除
+    flagExclude = juiceExclude(script_name)
+
+    if(!flagExclude){   // 不排除的任务就进行修改
+      console.log("✨ 原定时任务：" , script_name, " 定时时间：", hour,":",  minute)
+      cronArray.push(task) // 加入任务列表
+    }
+
+    if(script_name == "PUSH"){  // 记录PUSH的推送脚本的时间
+      // console.log("记录PUSH推送时间")
+      pushHM[0] = parseInt(hour)
+      pushHM[1] = parseInt(minute)
+      // console.log(pushHM[0], pushHM[1])
+    }
+  }
+  sleep(5000)
 }
 
-function writeTask(){
-  createSheet(sheetNameCron)
-  flagConfig = ActivateSheet(sheetNameCron); // 激活cron配置表
-  // 主配置工作表存在
-  if (flagConfig == 1) {
-    // console.log(taskArray.length)
-    console.log("🍳 开始读取CRON配置表");
-    let pos = 1
-    // 写入表头
-    Application.Range(colNum[0] + pos).Value = "文档名"
-    Application.Range(colNum[1] + pos).Value = "文档id"
-    Application.Range(colNum[2] + pos).Value = "任务名"
-    Application.Range(colNum[3] + pos).Value = "任务id"
-    Application.Range(colNum[4] + pos).Value = "脚本id"
-    Application.Range(colNum[5] + pos).Value = "是否调整"
-    Application.Range(colNum[6] + pos).Value = "时间范围"
-    Application.Range(colNum[7] + pos).Value = "当前定时时间"
-    Application.Range(colNum[8] + pos).Value = "额外参数"
-    // taskArray.push({
-    //     "filename" : name,
-    //     "fileid" : fileid,
-    //     "script_id" : script_id,
-    //     "script_name" : script_name,
-    //     "task_id" : task_id,
-    //     "cron_type":cron_type,
-    //     "day_of_month": day_of_month,
-    //     "day_of_week": day_of_week,
-    //     "hour"  : hour,
-    //     "minute" : minute,
-    //   })
-
-    // console.log(taskArray)
-    // 写入获取到的定时任务数据
-    for(let i = 0; i < taskArray.length; i++){
-      pos = i + 2
-      let j = 0
-      Application.Range(colNum[0] + pos).Value = taskArray[i]["filename"]
-      Application.Range(colNum[1] + pos).Value = taskArray[i]["fileid"]
-      Application.Range(colNum[2] + pos).Value = taskArray[i]["script_name"]
-      Application.Range(colNum[3] + pos).Value = taskArray[i]["task_id"]
-      Application.Range(colNum[4] + pos).Value = taskArray[i]["script_id"]
-      Application.Range(colNum[5] + pos).Value = "否"
-      Application.Range(colNum[6] + pos).Value = "0~23"
-      Application.Range(colNum[7] + pos).Value = taskArray[i]["hour"] + ":" + taskArray[i]["minute"]
-      Application.Range(colNum[8] + pos).Value = taskArray[i]["cron_type"] + "&" + taskArray[i]["day_of_month"] + "&" + taskArray[i]["day_of_week"]
-    }
-  }
+// 写入最新的时间
+function writeNewTime(pos, hour, minute){
+  Application.Range("H" + pos).Value = hour + ":" + minute
 }
 
-function initCron(){
-  try{
-    Application.Sheets.Item('CRON').Delete()  // 为了获得最新数据，删除CRON表
-    storeWorkbook()
-  }catch{
-    console.log("🍳 不存在CRON表，开始进行创建")
+
+// 修改定时任务
+function putTask(url, headers, data, task_id, script_name){
+  let flagResult = 0
+  // console.log(url + "/" + task_id)
+  // 查看定时任务
+  resp = HTTP.put(
+    url + "/" + task_id,
+    data,
+    { headers: headers }
+  );
+  resp = resp.json()
+  // console.log(resp)
+
+  // {"result":"ok"}
+  // {"errno":10000,"msg":"value of hour is out of 24's bounds","reason":"","result":"InvalidArgument"}
+  result = resp["result"]
+  if(result == "ok"){
+    console.log("🎉 " + script_name + " 任务时间调整成功")
+    flagResult = 1
+  }else{
+    msg = resp["msg"]
+    console.log("📢 " , msg)
   }
-  
-  // 获取file_id
-  url = "https://drive.kdocs.cn/api/v5/roaming?count=" + count  // 只对前20条进行判断
-  getFile(url)
-  writeTask()
-
-  console.log("✨ 已完成对CRON表的写入，请到CRON表进行配置")
-  console.log("✨ 然后将CRON.js脚本加入定时任务，即可自动调整定时任务时间")
-}
-
-// 判断表格行列数，并记录目前已写入的表格行列数。目的是为了不覆盖原有数据，便于更新
-function determineRowCol() {
-  for (let i = 1; i < maxRow; i++) {
-    let content = Application.Range("A" + i).Text
-    if (content == "")  // 如果为空行，则提前结束读取
-    {
-      row = i - 1;  // 记录的是存在数据所在的行
-      break;
-    }
-  }
-  // 超过最大行了，认为row为0，从头开始
-  let length = colNum.length
-  for (let i = 1; i <= length; i++) {
-    content = Application.Range(colNum[i - 1] + "1").Text
-    if (content == "")  // 如果为空行，则提前结束读取
-    {
-      col = i - 1;  // 记录的是存在数据所在的行
-      break;
-    }
-  }
-  // 超过最大行了，认为col为0，从头开始
-
-  // console.log("✨ 当前激活表已存在：" + row + "行，" + col + "列")
-}
-
-// 统一编辑表函数
-function editConfigSheet(content) {
-  determineRowCol();
-  let lengthRow = content.length
-  let lengthCol = content[0].length
-  if (row == 0) { // 如果行数为0，认为是空表,开始写表头
-    for (let i = 0; i < lengthCol; i++) {
-      Application.Range(colNum[i] + 1).Value = content[0][i]
-    }
-
-    row += 1; // 让行数加1，代表写入了表头。
-  }
-
-  // 从已写入的行的后一行开始逐行写入数据
-  // 先写行
-  for (let i = 1 + row; i <= lengthRow; i++) {  // 从未写入区域开始写
-    for (let j = 0; j < lengthCol; j++) {
-      Application.Range(colNum[j] + i).Value = content[i - 1][j]
-    }
-  }
-  // 再写列
-  for (let j = col; j < lengthCol; j++) {
-    for (let i = 1; i <= lengthRow; i++) {  // 从未写入区域开始写
-      Application.Range(colNum[j] + i).Value = content[i - 1][j]
-    }
-  }
-}
-
-// 创建wps表
-function createWpsConfig(){
-  createSheet(sheetNameSubConfig) // 若wsp表不存在创建wps表
-  let flagExitContent = 1
-
-  if(ActivateSheet(sheetNameSubConfig)) // 激活cron配置表
-  {
-    // wps表内容
-    let content = [
-      ['wps_sid', '任务配置表超链接',],
-      ['此处填写wps_sid', '点击此处跳转到CRON表',]
-    ]
-    determineRowCol() // 读取函数
-    if(row <= 1){ // 说明是空表或只有表头未填写内容
-      // console.log(row)
-      flagExitContent = 0 // 原先不存在内容，告诉用户先填内容
-      editConfigSheet(content)
-      // console.log(row)
-      let name = "点击此处跳转到CRON表"  // 'CRON'!A1
-      let link = "CRON"
-      let link_name ='=HYPERLINK("#'+link+'!$A$1","'+name+'")' //设置超链接
-      //console.log(link_name)  // HYPERLINK("#PUSH!$A$1","PUSH")
-      Application.Range("B2").Value = link_name
-
-    }
-  }
-
-  return flagExitContent
-  
+  sleep(5000)
+  return flagResult
 }
 
 
 function main(){
-  storeWorkbook()
-  let flagExitContent = createWpsConfig()
-  if(flagExitContent == 0){
-    console.log("📢 请先填写wps表，然后再运行一次此脚本")
-  }else{
-    wps_sid = getWpsSid() // 获取wps_sid
-    cookie = "wps_sid=" + wps_sid // 获取cookie
-
-    headers = {
-      "Cookie": cookie,
-      "Content-Type" : "application/json",
-      "Origin":"https://www.kdocs.cn",
-      "Priority":"u=1, i",
-    }
-    
-    
-    // 获取定时任务,生成CRON定时任务表
-    initCron()
-
+  
+  getWpsSid() // 获取cookie
+  headers= {
+    "Cookie": cookie,
+    "Content-Type" : "application/json",
+    "Origin":"https://www.kdocs.cn",
+    "Priority":"u=1, i",
+  //   "Content-Type":"application/x-www-form-urlencoded",
   }
+  // console.log(headers)
+
+  
+  // 设置定时任务
+  ActivateSheet(sheetNameCron);
+
+  let file_name = ""
+  let file_id = ""
+  let task_name = ""
+  let script_name = ""
+  let task_id  = ""
+  let script_id = ""
+  let exclude = ""
+  let hmrange = ""
+  let hm = ""
+  let hour = 0
+  let minute = 0
+  let extra = ""
+  let cron_type = ""
+  let day_of_month = ""
+  let day_of_week = ""
+  for (let i = 2; i <= line; i++) {
+      file_name = Application.Range("A" + i).Text;
+      if (file_name == "") {
+          // 如果为空行，则提前结束读取
+          break;
+      }
+
+      exclude = Application.Range("F" + i).Text;  // 是否调整
+
+      if (exclude == "是") {  // 是代表进行调整，则进行修改
+        file_id = Application.Range("B" + i).Value;
+        // console.log(file_id)
+        task_name = Application.Range("C" + i).Text;
+        script_name = task_name
+        task_id = Application.Range("D" + i).Text;
+        script_id = Application.Range("E" + i).Text;
+        hmrange = Application.Range("G" + i).Text;
+        hm = Application.Range("H" + i).Text;
+        extra = Application.Range("I" + i).Text;
+        console.log("🧑 开始任务修改：" , file_name, "-", task_name )
+        let keyarry= hm.split(":") // 使用|作为分隔符
+
+        hour = parseInt(keyarry[0])
+        minute = parseInt(keyarry[1])
+        // rangeHM(hmrange)  // 范围限定函数
+        newHM = createTime(hour, minute, hmrange)
+        hour = newHM[0]
+        minute = newHM[1]
+
+        keyarry = extra.split("&") // 使用|作为分隔符
+        cron_type = keyarry[0]
+        day_of_month = keyarry[1]
+        day_of_week = keyarry[2]
+
+        // 进行时间修改
+        url = "https://www.kdocs.cn/api/v3/ide/file/" + file_id + "/cron_tasks";
+        // console.log(url)
+        data = {
+          "id": file_id,
+          "script_id": script_id,
+          "cron_detail": {
+              "task_type": "cron_task",
+              "cron_desc": {
+                  "cron_type": cron_type,
+                  "day_of_month": day_of_month,
+                  "day_of_week": day_of_week,
+                  "hour" : hour,
+                  "minute": minute
+              }
+          },
+          "task_id": task_id,
+          "status": "enable"
+        }
+        // console.log(data)
+
+        console.log("✨ 现定时任务：" , script_name, " 定时时间：", hour,":",  minute)
+        let flagResult = putTask(url, headers, data, task_id, script_name)
+        if(flagResult){ // 时间修改成功
+          let pos = i
+          writeNewTime(pos, hour, minute)
+        }
+
+      }
+  } 
 
 }
 
